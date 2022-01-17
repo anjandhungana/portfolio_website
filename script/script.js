a70aefc75e2418091515e7d91f4e0d5d574d8292
Anjan Dhungana <anjan2052@gmail.com>
2022-01-17 18:56:54 +0545

    adds scroll progress indicator at the top

--- script/script.js
+++ script/script.js
@@ -10,4 +10,25 @@ const clicknav = (obj) =>{
     // console.log(obj.getAttribute('id'));
     id = obj.getAttribute('id');
     document.getElementById(id).classList.add('active');
+    // console.log(document.)
 }
+
+
+// progress
+document.addEventListener('scroll',()=>{
+    var totalheight = document.documentElement.scrollHeight-document.documentElement.clientHeight;
+    var scrollheight = window.scrollY;
+    var height = scrollheight/totalheight*100;
+    var width = document.getElementById('sidebar-menu').clientWidth;
+    
+    var y = document.getElementById('progress').style.width = (height) + '%';
+    console.log(y)
+
+    // full scroll
+    if (height>15){
+        
+    }
+});
+
+
+
